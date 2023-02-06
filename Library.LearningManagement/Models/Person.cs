f7fb378aa0ed1fceb33263d3941174eff82008f2
Kaleb Szaro <kms19t@my.fsu.edu>
2023-02-06 14:23:16 -0500

    Initial Commit - Closes #4

---
+++ Library.LearningManagement/Models/Person.cs
@@ -0,0 +1,21 @@
+﻿namespace Library.LearningManagement.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public Dictionary<int, double> Grades { get; set; } //A list of assignments, each have ID (key into dictionary, double is grade)
+
+        public char Classification { get; set; }
+
+        public Person()
+        {
+            Name = string.Empty;    //set empty string instead of null
+            Grades = new Dictionary<int, double>();
+
+
+        }
+
+    }
+}
