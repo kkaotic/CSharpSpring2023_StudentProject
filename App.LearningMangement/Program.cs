ddf396cb099b7a9bf8076edfd3aec7bb61195c7b
Kaleb Szaro <kms19t@my.fsu.edu>
2023-02-07 09:57:54 -0500

    Created Update Course functionality. Closes #23

--- App.LearningMangement/Program.cs
+++ App.LearningMangement/Program.cs
@@ -20,8 +20,9 @@ namespace MyApp
                 Console.WriteLine("[3] List all enrolled Students");
                 Console.WriteLine("[4] Search for a student");
                 Console.WriteLine("[5] Create a new course");
-                Console.WriteLine("[6] List all courses");
-                Console.WriteLine("[7] Exit");
+                Console.WriteLine("[6] Update a course");
+                Console.WriteLine("[7] List all courses");
+                Console.WriteLine("[8] Exit");
                 var input = Console.ReadLine();
                 if (int.TryParse(input, out int result))
                 {
@@ -42,9 +43,13 @@ namespace MyApp
                         courseHelper.CreateCourseRecord();
                     }else if (result == 6)
                     {
-                        courseHelper.ListCourses();
+                        courseHelper.UpdateCourse();
                     }
                     else if (result == 7)
+                    {
+                        courseHelper.ListCourses();
+                    }
+                    else if (result == 8)
                     {
                         cont = false;
                     }
